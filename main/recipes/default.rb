a721c2a417c6ad94d1bd79124940f1fec8c49add
Ravi Jangid <ravi.jangid@watchmaster.com>
2016-10-17 11:53:23 +0200

    Updated app URL

--- main/recipes/default.rb
+++ main/recipes/default.rb
@@ -8,3 +8,10 @@ execute 'ls -ld /db-data || mkdir /db-data'
 
 # Mount the NFS directory to this location
 execute "mount | grep db-data > /dev/null || mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).#{node[:nfs_endpoint]}:/ /db-data"
+
+template "/var/lib/tomcat7/conf/server.xml" do
+  source "server.xml.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
