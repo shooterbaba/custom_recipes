91e13b49f538a13d7c1ee4993641543308371b46
Ravi Jangid <ravi.jangid@watchmaster.com>
2016-10-17 15:30:07 +0200

    Condition for shared directory mount

--- main/recipes/default.rb
+++ main/recipes/default.rb
@@ -9,7 +9,7 @@ execute 'chown tomcat7:tomcat7 /Users/dcameron/persistence'
 
 # Mount the NFS directory to this location
 if not node[:nfs_endpoint].nil?
-   execute "mount | grep persistence > /dev/null || mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).#{node[:nfs_endpoint]}:/ /Users/dcameron/persistence"
+   execute "mount | grep persistence > /dev/null || mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).#{node[:nfs_endpoint]}.efs.#{node[:opsworks][:instance][:region]}.amazonaws.com:/ /Users/dcameron/persistence"
    execute "chown tomcat7:tomcat7 -R /Users/dcameron/persistence"
 end
 
